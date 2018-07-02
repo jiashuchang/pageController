d4fb89cd0fea6638d20d1e6778c6619c7368f633
TianLi <tianli@TianLideiMac.local>
2018-07-02 15:13:04 +0800

    Initial Commit

---
+++ uipageviewcontroller/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  uipageviewcontroller
+//
+//  Created by TianLi on 2018/7/2.
+//  Copyright © 2018年 TianLi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
